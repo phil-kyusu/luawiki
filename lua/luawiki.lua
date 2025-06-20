9d274d4977cd2b6f02d9cbb6f5baf0580df9c20b
Phil Labrum <phil@trymoab.com>
2025-06-19 23:07:21 -0400

    this is just vibe coded

--- lua/luawiki.lua
+++ lua/luawiki.lua
@@ -1,21 +1,16 @@
 local M = {}
-local function ind(s, i)
-    return string.sub(s, i, i)
-end
-local function starts(s, pre)
-   return string.sub(s,1,string.len(pre))==pre
-end
-local function ends(s, post)
-    return string.sub(s, -string.len(post))==post
-end
+
+-- Utility functions
+local function ind(s, i) return string.sub(s, i, i) end
+local function starts(s, pre) return string.sub(s, 1, #pre) == pre end
+local function ends(s, post) return string.sub(s, -#post) == post end
 
 local function replace_line(row, new)
-    vim.api.nvim_buf_set_lines(0, row-1, row, true, { new } )
+  vim.api.nvim_buf_set_lines(0, row - 1, row, true, { new })
 end
 
 local function keymap_a(mode, lhs, rhs, opts)
   opts = opts or { buffer = 0 }
-
   if type(rhs) == "function" then
     local orig = rhs
     rhs = function(...)
@@ -26,147 +21,78 @@ local function keymap_a(mode, lhs, rhs, opts)
       return orig(...)
     end
   end
-
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-local cr = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
-local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
-
-local function istodo(line)
-    -- 0 if no, 1 if empty, 2 if checked
-    -- second return value is number of spaces
-    local i = string.find(line, "-")
-    if i == nil then
-        return 0, 0
-    end
-    line = string.sub(line, i)
-    if starts(line, "- [ ] ") then
-        return 1, i - 1
-    elseif starts(line, "- [X] ") then
-        return 2, i - 1
-    end
-    -- unreachable
+-- Toggle TODO
+function M.todo()
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_buf_get_lines(0, row - 1, row, {})[1] or ""
+  local new = ""
+  local function istodo(line)
+    local i = line:find("-")
+    if not i then return 0, 0 end
+    local sub = line:sub(i)
+    if starts(sub, "- [ ] ") then return 1, i - 1 end
+    if starts(sub, "- [X] ") then return 2, i - 1 end
     return 0, i - 1
+  end
+
+  local todo_info, spaces = istodo(line)
+  if todo_info == 0 then
+    if line == "" then
+      replace_line(row, "- [ ] ")
+      vim.api.nvim_feedkeys("A", "n", false)
+    end
+    return
+  end
+
+  line = line:sub(spaces + 1)
+  new = todo_info == 1 and "- [X] " or "- [ ] "
+  new = string.rep(" ", spaces) .. new .. line:sub(7)
+  replace_line(row, new)
 end
 
-local function todo()
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line = vim.api.nvim_buf_get_lines(0, row-1, row, {})[1]
-    local new = ""
-    local todo_info, spaces = istodo(line)
+-- Continue TODO
+function M.continue_todo()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local line = vim.api.nvim_buf_get_lines(0, row - 1, row, {})[1]
+  local function istodo(line)
+    local i = line:find("-")
+    if not i then return 0, 0 end
+    local sub = line:sub(i)
+    if starts(sub, "- [ ] ") then return 1, i - 1 end
+    if starts(sub, "- [X] ") then return 2, i - 1 end
+    return 0, i - 1
+  end
 
-    if todo_info == 0 then
-        if line == "" then
-            replace_line(row, "- [ ] ")
-            vim.api.nvim_feedkeys("A", "n", false)
-        end
-        return
-    end
-
-    line = string.sub(line, spaces+1)
-    if todo_info == 1 then
-        new = "- [X] "
-    elseif todo_info == 2 then
-        new = "- [ ] "
-    else
-        return
-    end
-    local contents = string.sub(line, string.len("- [ ] ")+1)
-    new = string.rep(" ", spaces) .. new .. contents
-    replace_line(row, new)
-    -- vim.api.nvim_buf_set_text(0, row - 1, 0, row - 1, string.len(new), {new})
+  local res, spaces = istodo(line)
+  if res == 0 then return false end
+  local rmi = vim.api.nvim_replace_termcodes("cc0<C-D>", true, true, true)
+  if line:sub(spaces) == "- [ ] " then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true) .. rmi, "n", false)
+  else
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes("<CR><Esc>", true, true, true) ..
+      rmi .. string.rep(" ", spaces) .. "- [ ] ",
+      "n", false
+    )
+  end
+  return true
 end
 
-local function continue_todo()
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line = vim.api.nvim_buf_get_lines(0, row-1, row, {})[1]
-    local res, spaces = istodo(line)
-    if res == 0 then
-        return false
-    end
-    local t = string.sub(line, spaces)
-    
-    -- https://www.reddit.com/r/neovim/comments/xueish/comment/iqw5o8s
-    -- switch to buf set text at some point
-    local rmi = vim.api.nvim_replace_termcodes("cc0<C-D>", true, true, true)
-    if t == "- [ ] " then
-        -- hitting enter twice exits out of todo
-        vim.api.nvim_feedkeys(
-          esc .. rmi, "n", false
-        )
-    else
-        vim.api.nvim_feedkeys(
-          cr .. esc .. rmi .. string.rep(" ", spaces) .. "- [ ] ", "n", false
-        )
-    end
-
-    return true -- if continued
-end
-
-local defaults = {
-    filemaps = {
-        markdown = {
-            pattern = {"*.md"},
-            linkstart = "[",
-            linkstop = ")",
-            getlink = function(block)
-                -- swap ro regex later
-                local t = string.find(block, "%(")
-                return string.sub(block, t+1, string.len(block) - 1)
-            end,
-            formatlink = function(block)
-                return "[" .. block .. "](" .. block .. ".md)"
-            end,
-            extras = function()
-                -- todo shortcuts
-                keymap_a('n', '<S-CR>', function() todo() end)
-                keymap_a('i', '<CR>', function()
-                    if not continue_todo() then
-                        return '<CR>'
-                    end
-                end,
-                    { expr = true, noremap = true, buffer = 0 }
-                )
-            end
-        },
-        wiki = {
-            pattern = {"*.wiki"},
-            linkstart = "[[",
-            linkstop = "]]",
-            getlink = function(block)
-                local word = string.sub(block, 3, string.len(block) - 2)
-                if not ends(word, ".md") then
-                    word = word .. ".wiki"
-                end
-
-                return word
-            end,
-            formatlink = function(block)
-                return "[[" .. block .. "]]"
-            end
-        }
-    }
-}
-
--- Local state
-local Config = {}
+-- Navigation
 local Depth = {}
-
--- Link and go back
-local function link(filetype)
+function M.link(filetype)
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local line = vim.api.nvim_buf_get_lines(0, row - 1, row, {})[1]
   local s, e = col, col + 1
-  local filemap = Config.filemaps[filetype]
+  local filemap = M.config.filemaps[filetype]
 
   while s > 0 and ind(line, s) ~= ' ' do s = s - 1 end
   while e <= #line and ind(line, e) ~= ' ' do e = e + 1 end
 
-  local block = string.sub(line, s, e)
-  if starts(block, " ") then block = string.sub(block, 2) end
-  if ends(block, " ") then block = string.sub(block, 1, #block - 1) end
-
+  local block = line:sub(s, e):gsub("^%s+", ""):gsub("%s+$", "")
   if starts(block, filemap.linkstart) and ends(block, filemap.linkstop) then
     table.insert(Depth, vim.api.nvim_buf_get_name(0))
     vim.cmd("e " .. filemap.getlink(block))
@@ -175,24 +101,58 @@ local function link(filetype)
   end
 end
 
-local function goback()
-  if #Depth > 0 then
-    vim.cmd("e " .. table.remove(Depth))
-  end
+function M.goback()
+  if #Depth > 0 then vim.cmd("e " .. table.remove(Depth)) end
 end
 
 -- Setup
-function M.setup(user_opts)
-  Config = vim.tbl_deep_extend("force", {}, defaults, user_opts or {})
+M.config = {}
+local defaults = {
+  mappings = {
+    todo = "<S-CR>",
+    backwards = "<BS>",
+    follow = "<CR>",
+  },
+  filemaps = {
+    markdown = {
+      pattern = { "*.md" },
+      linkstart = "[",
+      linkstop = ")",
+      getlink = function(b)
+        local i = b:find("%(")
+        return b:sub(i + 1, #b - 1)
+      end,
+      formatlink = function(b) return "[" .. b .. "](" .. b .. ".md)" end,
+    },
+    wiki = {
+      pattern = { "*.wiki" },
+      linkstart = "[[",
+      linkstop = "]]",
+      getlink = function(b)
+        local word = b:sub(3, #b - 2)
+        return ends(word, ".md") and word or word .. ".wiki"
+      end,
+      formatlink = function(b) return "[[" .. b .. "]]" end,
+    }
+  }
+}
 
-  for filetype, tbl in pairs(Config.filemaps) do
+function M.setup(user_opts)
+  M.config = vim.tbl_deep_extend("force", {}, defaults, user_opts or {})
+
+  for filetype, tbl in pairs(M.config.filemaps) do
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
       pattern = tbl.pattern,
       callback = function()
         vim.schedule(function()
-          keymap_a('n', '<CR>', function() link(filetype) end)
-          keymap_a('n', '<BS>', goback)
-          if tbl.extras then tbl.extras() end
+          keymap_a("n", M.config.mappings.follow, function() M.link(filetype) end)
+          keymap_a("n", M.config.mappings.backwards, M.goback)
+          if M.config.mappings.todo then
+            keymap_a("n", M.config.mappings.todo, M.todo)
+            keymap_a("i", "<CR>", function()
+              if not M.continue_todo() then return "<CR>" end
+            end, { expr = true, noremap = true, buffer = 0 })
+          end
         end)
       end
     })
